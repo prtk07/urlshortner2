0683524e92b9d8e75086ae97218cd927aee62f33
Prateek Verma <prateekverma70@gmail.com>
2022-01-19 11:21:51 +0530

    fix:minor structural changes

--- dist/url.models.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var URLSchema = new mongoose_1.Schema(require("../url.schema.json"));
-var url = (0, mongoose_1.model)("URL", URLSchema);
-exports.default = url;
